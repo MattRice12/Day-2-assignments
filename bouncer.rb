cf555efc94ede8252e0251b3d3e678e93c1e6a32
Matt Rice <mattrice12@outlook.com>
2016-06-14 14:52:26 -0500

    Added prompt for user to play again or quit. Added response for people who claim to be older than the oldest person of record.

--- bouncer.rb
+++ bouncer.rb
@@ -13,32 +13,70 @@ def check_age
   gets.chomp.to_i
 end
 
-under_eighteen = ["do nothing"]
-over_eighteen = ["smoke", "vote"]
-over_twenty_one = [over_eighteen, "drink"]
-over_twenty_five = [over_twenty_one, "rent a car"]
-
-age = check_age
-loop do
-if (age < 18) && (age > 1)
-  puts "You can #{under_eighteen.join}."
-  break
-elsif (age >= 18) && (age < 21)
-  last_elem = over_eighteen.pop
-  puts "You can #{over_eighteen.join} and #{last_elem}."
-  break
-elsif (age >= 21) && (age < 25)
-  last_elem = over_twenty_one.pop
-  puts "You can #{over_twenty_one.join(", ")}, and #{last_elem}."
-  break
-elsif age >= 25
-  last_elem = over_twenty_five.pop
-  puts "You can #{over_twenty_five.join(", ")}, and #{last_elem}."
-  break
-else
-  puts "That is not an age."
-  puts "Tell me your REAL age!"
+def get_user_input
   puts
-  age = check_age
+  puts "What would you like to do?"
+  print "<(p)lay again or (q)uit> "
+
+  gets.chomp #gets user input to store in variable response further down
 end
+
+def unknown_input
+  puts "Sorry, I didn't catch that?"
+end
+
+
+
+def game # this is the game method
+  under_eighteen = ["do nothing"]
+  over_eighteen = ["smoke", "vote"]
+  over_twenty_one = [over_eighteen, "drink"]
+  over_twenty_five = [over_twenty_one, "rent a car"]
+
+  system("clear")
+
+  age = check_age
+
+  loop do # this loop returns messages based on age input
+    if (age < 18) && (age > 1) # Under 18
+      puts "You can #{under_eighteen.join}."
+      break
+    elsif (age >= 18) && (age < 21) # Between 18-21
+      last_elem = over_eighteen.pop
+      puts "You can #{over_eighteen.join} and #{last_elem}."
+      break
+    elsif (age >= 21) && (age < 25) # Between 21-25
+      last_elem = over_twenty_one.pop
+      puts "You can #{over_twenty_one.join(", ")}, and #{last_elem}."
+      break
+    elsif (age >= 25) && (age <= 122)# Over 25
+      last_elem = over_twenty_five.pop
+      puts "You can #{over_twenty_five.join(", ")}, and #{last_elem}."
+      break
+    elsif (age > 122)
+      puts "You're older than the oldest person who ever lived. You've \
+unlocked god mode."
+      break
+    else # Reprompts the user when age < 0 or user inputs words.
+      puts "That is not an age."
+      puts "Tell me your REAL age!"
+      puts
+      age = check_age
+    end
+  end
+end
+game
+
+loop do # This loop prompts the player if he wants to play again
+  response = get_user_input
+
+  case response
+    when 'p', 'play', 'play again'
+      puts
+      game
+    when 'q', 'quit' then break
+    else
+      puts
+      unknown_input
+  end
 end
